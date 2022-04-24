042cf9ad3dcc99c34b9783c81c4ca17f5ee0e7f1
Ryzhik Daniel <67552741+ZinixZay@users.noreply.github.com>
2022-04-24 19:28:21 +0300

    First_version

---
+++ config.py
@@ -0,0 +1,20 @@
+bot_token = '5322018705:AAEXruzN5LSuQUyyNBSYwIhLuv_w3pOyaU0'
+bot2_token = '5349953088:AAHomwTpZQIT6q03KMCXFsdeKk0PKZuqA5k'
+owm_token = '917852749a2cb4798dfc27e83acacd89'
+weather_commands = ['погода', 'погоду', 'weather', 'температура', 'погодка', 'п', 'w']
+starting_commands = ['start', 'старт', 's', 'с']
+mailing_commands = ['таймер', 'timer', 't', 'т']
+info_commands = ['help', 'info', 'FAQ', 'faq', 'помощь', 'команды', 'инфо']
+firebaseConfig = {
+  'apiKey': "AIzaSyD9EbsxWwfKSn-qmy801YJFBbhU7N3uth4",
+  'authDomain': "telegram-9e8d3.firebaseapp.com",
+  'databaseURL': "https://telegram-9e8d3-default-rtdb.firebaseio.com",
+  'projectId': "telegram-9e8d3",
+  'storageBucket': "telegram-9e8d3.appspot.com",
+  'messagingSenderId': "975115353520",
+  'appId': "1:975115353520:web:34d5c3507ed5f4f0cb6a75",
+  'measurementId': "G-9DNTFEK0ZM"
+}
+mailing_delete = ['удалить', 'убрать', 'у', 'delete', 'd']
+rzd = [',', '.', ':', ' ']
+secret_code = 'kAr8932mV_AcasklogporEER'
